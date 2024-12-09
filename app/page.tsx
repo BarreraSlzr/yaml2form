f9b9a8aa387e238504a123e7644f65ab38ffe355
Emmanuel Barrera <barreraslzr@gmail.com>
2024-12-08 18:39:02 -0600

    Improve ux for mobile page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import YamlToForms from '@/components/yaml-to-forms'
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 overflow-auto max-h-screen">
       <h1 className="text-2xl font-bold mb-4">YAML to Forms</h1>
       <YamlToForms />
     </div>
